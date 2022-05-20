2ce1f0510010fdacbfdff86f5a6830292e6c3b97
kevin-vargas <kevinvargasnavia@gmail.com>
2022-05-19 22:29:50 -0300

    add mine server

--- registry/create-secret-image-pull.sh
+++ registry/create-secret-image-pull.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 SECRET_NAME=regcred
-NAMESPACE=kevin
 ask_values(){
     read -p 'Server: ' SERVER_INPUT
     read -p 'Username: ' USERNAME_INPUT
+    read -p 'Namespace: ' NAMESPACE_INPUT
     read -sp 'Password: ' PASSWORD_INPUT
 }
 
 create_secret(){
     local NAMESPACE_SECRET
 
-    if [ ! -z ${NAMESPACE+x} ];
+    if [ ! -z ${NAMESPACE_INPUT+x} ];
     then
-        NAMESPACE_SECRET="--namespace=${NAMESPACE}"
+        NAMESPACE_SECRET="--namespace=${NAMESPACE_INPUT}"
     fi
 
     kubectl create secret docker-registry $SECRET_NAME --docker-server=$SERVER_INPUT --docker-username=$USERNAME_INPUT --docker-password=$PASSWORD_INPUT ${NAMESPACE_SECRET}
